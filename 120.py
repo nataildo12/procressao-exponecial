a70260707c586f34f9b23e28b91d90a9ac7ebd66
nataildo12 <61765993+nataildo12@users.noreply.github.com>
2020-03-03 22:24:56 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,10 @@
+#autores:nataildo
+#feito em:03/04/20202
+#istituição: Etec
+
+x = 0  #Declaração de comando
+valor = 2 #Declaração de variavel valor
+while (x <= 21);
+	    valor = valor * valor
+		 x =x 1 = #laço de repedição
+print (valor) # exibir variavel de valor
